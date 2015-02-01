4abed3a33fcc186d9eb59f2e06e89b07c5a5a521
Mateusz Czajka <mateusz.czajka@netguru.pl>
2015-02-01 12:23:44 +0100

    always show correct answers

--- learn.rb
+++ learn.rb
@@ -10,21 +10,21 @@ def main
   while true
     question = next_question(data)
     puts question["text"].colorize(:light_blue)
-    
+
     correct_answers =print_answers(question)
     #puts correct_answers.to_s.colorize(:light_red) #debug
     input =  gets.chomp
     system "clear"
     if correct?( correct_answers, input)
       puts "OK".colorize(:green)
-    else 
+    else
       puts "NOT OK".colorize(:red)
-      puts question["text"].colorize(:light_blue)
-      print_correct(question);
-      print "Press any key"
-      gets.chomp
-      system "clear"
     end
+    puts question["text"].colorize(:light_blue)
+    print_correct(question);
+    print "Press any key"
+    gets.chomp
+    system "clear"
   end
 end
 
@@ -43,7 +43,7 @@ def print_answers(question)
   n = question["correct"].length + question["wrong"].length
   tmp = Range.new(0, n -1).to_a.shuffle
   answers = Array.new(n-1)
-    
+
   result = []
   question["correct"].each do |a|
     answers[tmp[i]] = a
@@ -54,13 +54,13 @@ def print_answers(question)
     answers[tmp[i]] = a
     i = i +1
   end
-  
+
   i = 0
   answers.each do |a|
     puts "#{i})\t#{a}"
     i = i + 1
   end
-  
+
   result
 end
 
